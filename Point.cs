807d2a8dfa571dab51719344524f16d93fbc0a75
USER <tamimachy9897@gmail.com>
2025-12-12 22:52:55 +0600

    Methods updated

---
+++ Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y; 
+        }
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y; 
+        }
+        public void Move(Point newLocation)
+        {
+            if(newLocation == null)
+                    throw new ArgumentException("newLocation cannot be null.");
+
+            Move(newLocation.X, newLocation.Y);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,20 +8,67 @@ namespace Program
 {
     class Program
     {
+        public class Calculator
+        {
+            public int Add(params int[] numbers)
+            {
+                var sum = 0;
+                foreach (var number in numbers)
+                    sum += number;
+
+                return sum;
+            }
+        }
         static void Main(string[] args)
         {
             // var person = Person.Parse("John");
-            var customer = new Customer();
-            customer.Id = 1;
-            customer.Name = "Mosh";
+            //var customer = new Customer();
+            //customer.Id = 1;
+            //customer.Name = "Mosh";
 
-            var order = new Order();
-            customer.Orders = new List<Order>();
-            customer.Orders.Add(order);
+            //var order = new Order();
+            //customer.Orders = new List<Order>();
+            //customer.Orders.Add(order);
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            //Console.WriteLine(customer.Id);
+            //Console.WriteLine(customer.Name);
             //person.Introduce("Mosh");
+
+            //var number = int.Parse("abc");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed.");
+
+
+
+        }
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4, 5));
+
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occurred.");
+            }
         }
     }
 }
